dd2ed3a4e6387b3ef2c145822789b9efd30421f6
AryanYadav12342004 <as9719kk@gmail.com>
2025-07-01 21:10:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';  // Import the Dashboard page
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />  {/* Dashboard route */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
